5fb9138ea356e1411ad802ce5edf1869dde433bc
alirex <alirex.prime@gmail.com>
2025-11-14 18:38:54 +0200

    "Refactor `contact_view` rendering logic by introducing `render_item_card_with_panel` helper"
    - Moved reusable rendering logic to the new `render_item_card_with_panel` for better modularity and separation of concerns.
    - Simplified `contact_render_details` by leveraging the centralized helper.
    - Removed redundant imports and unused code for improved clarity.

---
+++ src/persyval/services/console/render_item_card_with_panel.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING, Final
+
+from pydantic import BaseModel
+from rich.markup import escape
+
+from persyval.services.console.types import RichFormattedText
+from persyval.utils.format import render_with_panel
+
+if TYPE_CHECKING:
+    from rich.console import Console
+
+
+class RenderItem(BaseModel):
+    name: str
+    value: str | None
+
+    no_escape_value: bool = False
+
+
+RENDER_I_PLACEHOLDER: Final[str] = "-"
+RENDER_I_STYLE: Final[str] = "green bold"
+
+
+def render_item_card_with_panel(
+    console: Console,
+    list_to_render: list[RenderItem],
+) -> None:
+    message_as_list = []
+    for item in list_to_render:
+        value = item.value if item.value is not None else RENDER_I_PLACEHOLDER
+        if not item.no_escape_value:
+            value = escape(value)
+
+        part = f"[{RENDER_I_STYLE}]{escape(item.name)}:[/{RENDER_I_STYLE}] {value}"
+        message_as_list.append(part)
+
+    message = "\n".join(message_as_list)
+
+    render_with_panel(
+        console=console,
+        title="Contact details",
+        message=RichFormattedText(message),
+        style=RENDER_I_STYLE,
+    )
--- src/persyval/services/handlers/contact_view.py
+++ src/persyval/services/handlers/contact_view.py
@@ -1,7 +1,4 @@
-from typing import TYPE_CHECKING, Final
-
-from pydantic import BaseModel
-from rich.markup import escape
+from typing import TYPE_CHECKING
 
 from persyval.models.contact import (
     Contact,
@@ -9,11 +6,10 @@ from persyval.models.contact import (
 )
 from persyval.services.birthday.parse_and_format import format_birthday_for_output
 from persyval.services.commands.command_meta import ArgMetaConfig, ArgsConfig
-from persyval.services.console.types import RichFormattedText
+from persyval.services.console.render_item_card_with_panel import RenderItem, render_item_card_with_panel
 from persyval.services.data_actions.contact_get import contact_get
 from persyval.services.execution_queue.execution_queue import HandlerArgsBase
 from persyval.services.handlers_base.handler_base import HandlerBase
-from persyval.utils.format import render_with_panel
 
 if TYPE_CHECKING:
     from rich.console import Console
@@ -54,35 +50,18 @@ class ContactViewIHandler(
         )
 
 
-class RenderItem(BaseModel):
-    name: str
-    value: str
-
-
-RENDER_I_PLACEHOLDER: Final[str] = "-"
-RENDER_I_STYLE: Final[str] = "green bold"
-
-
 def contact_render_details(console: Console, contact: Contact) -> None:
-    list_to_render: list[RenderItem] = [
-        RenderItem(name="Name", value=contact.name),
-        RenderItem(name="Address", value=contact.address or RENDER_I_PLACEHOLDER),
-        RenderItem(
-            name="Birthday",
-            value=format_birthday_for_output(contact.birthday) if contact.birthday else RENDER_I_PLACEHOLDER,
-        ),
-        RenderItem(name="Phones", value=", ".join(contact.phones) if contact.phones else RENDER_I_PLACEHOLDER),
-        RenderItem(name="Emails", value=", ".join(contact.emails) if contact.emails else RENDER_I_PLACEHOLDER),
-        RenderItem(name="Uid", value=str(contact.uid)),
-    ]
-
-    message = "\n".join(
-        [f"[{RENDER_I_STYLE}]{escape(item.name)}:[/{RENDER_I_STYLE}] {escape(item.value)}" for item in list_to_render],
-    )
-
-    render_with_panel(
+    render_item_card_with_panel(
         console=console,
-        title="Contact details",
-        message=RichFormattedText(message),
-        style=RENDER_I_STYLE,
+        list_to_render=[
+            RenderItem(name="Name", value=contact.name),
+            RenderItem(name="Address", value=contact.address),
+            RenderItem(
+                name="Birthday",
+                value=format_birthday_for_output(contact.birthday) if contact.birthday else None,
+            ),
+            RenderItem(name="Phones", value=", ".join(contact.phones) if contact.phones else None),
+            RenderItem(name="Emails", value=", ".join(contact.emails) if contact.emails else None),
+            RenderItem(name="Uid", value=str(contact.uid)),
+        ],
     )
